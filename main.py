5f95fdf7c1c742e35bc78d1cfd9dea51a63a2001
Dann Starr <dannstarr@gmail.com>
2024-02-28 20:00:31 +0000

    changed if / elif to match case

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 todos = []
 print("Welcome to your To-Do List!")
-print("Type 'add' to add a new todo, 'show' to show your todo's, or Enter 'q' to quit.")
+print("Type 'add' to add a new todo, 'show' to show your todo's, or enter 'q' to quit.")
 
 while True:
-    command = input("Enter a command: ").strip()
-    if command == 'q':
-        print("Uploading virus... \nPlease wait... \nvirus upload complete")
-        break
-    elif command == "add":
-        todo = input("Enter ToDo item: ")
-        todos.append(todo)
-    elif command == "show":
-        for item in todos:
-            print(item)
-    else:
-        print("Please enter a valid command")
+    command = input("Enter a command: ").strip().lower()
+    match command:
+        case "exit":
+            print("Uploading virus... \nPlease wait... \nvirus upload complete")
+            break
+        case "add":
+            todo = input("Enter ToDo item: ")
+            todos.append(todo)
+            print("Todo added successfully!")
+        case "show" | "display":
+            if todos:
+                print("Your To-Do List:")
+                for item in todos:
+                    print(item)
+            else:
+                print("Your To-Do List is empty.")
+        case _:
+            print("Please enter a valid command")
+
